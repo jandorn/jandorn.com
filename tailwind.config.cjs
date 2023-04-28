a38f392b116b3446e03b9f1bf7550a2087779512
Jan <ja.dorn@web.de>
2023-04-28 11:59:58 +0200

    tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				light: "#F0F0F0",
+				dark: "#0F1212",
+				accent: "#52647A",
+				text: "#52647A",
+			},
+		},
+	},
+	plugins: [],
+}
