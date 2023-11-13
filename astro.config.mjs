5b9e79818b4fd21d48cbc329d930229f7dbdfb3d
Jan <ja.dorn@web.de>
2023-11-13 14:44:07 +0100

    enable astro pre fetching for the entire site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jandorn.com',
+  prefetch: {
+    prefetchAll: true
+  },
   integrations: [tailwind({
     applyBaseStyles: false
   }), vue(), sitemap()]
